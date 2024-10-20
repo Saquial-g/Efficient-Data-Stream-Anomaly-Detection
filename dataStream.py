77720372d6ef2c5f66d7c5af7c491c2bccd4c72a
Saquial <misamyquintero@gmail.com>
2024-10-20 14:40:56 -0500

    Data stream
    Polishing of the data stream and the visual interface. Error handling added for data generation and memory leaks.

--- dataStream.py
+++ dataStream.py
@@ -1,12 +1,24 @@
 import random
 import math
 
+#complete data flow, can be used by the program to analize historical patterns
 dataArray = []
 
 def genStream(t):
-    pattern = 1000 + 100 * math.sin(2 * math.pi * t/100)
-    trend = 0
-    variation = random.uniform(-200,200)
-    point = pattern + variation + trend
-    dataArray.append(point)
+    #generate the next data point
+    pattern = dataArray[-1] + 5 * math.sin(2 * math.pi * t/50)
+    trend = 1
+    noise = random.uniform(-18,18)
+    point = pattern + noise + trend
+
+    #Add the data point to the data stream while avoiding negative values
+    if dataArray[-1] + point < 0:
+        dataArray.append(abs(point))
+    else:
+        dataArray.append(point)
+
+    #handle memory leak
+    if len(dataArray) > 100000:
+        del dataArray[0]
+
     return point
--- visualizer.py
+++ visualizer.py
@@ -4,29 +4,45 @@ from matplotlib.animation import FuncAnimation
 import dataStream as ds
 
 class flowGraph():
-    def __init__(self, data=[], anomalies=[]):
-        self.data = data
+    #Generates the initial graph. Displays provided data, but can be initialized empty
+    def __init__(self, data=[300], anomalies=[], xsize = 100, limitXSize = True):
+        #Save the provided data in the local variables
+        ds.dataArray = data
         self.anomalies = anomalies
 
+        #Define the amount of values visible on the graph, could be setup by the user
+        self.xsize = xsize
+        self.limitXSize = limitXSize
+        
+        #Create the initial graph
         self.fig, self.ax = plt.subplots()
         self.line, = self.ax.plot([], [])
         self.anomaly_points, = self.ax.plot([], [], 'ro')  # red circles for anomalies
+        plt.xlabel("Time")
+        plt.ylabel("Value")
 
+        #Check for data provided, start with default values otherwise
         if (len(data) > 0):
-            self.ax.set_xlim(0, len(self.data))
-            self.ax.set_ylim(min(self.data) - 5, max(self.data) + 5)
+            self.ax.set_xlim(0, len(ds.dataArray))
+            self.ax.set_ylim(min(ds.dataArray) - 5, max(ds.dataArray) + 5)
+        else:
+            self.ax.set_xlim(0, 50)
+            self.ax.set_ylim(0, 1000)
         
-    
     def update(self, frame):
-        self.data.append(ds.genStream(frame))
-        self.line.set_data(range(len(self.data)), self.data)
+        ds.genStream(frame)
+        if len(ds.dataArray) > self.xsize and self.limitXSize:
+            self.line.set_data(range(len(ds.dataArray) - self.xsize, len(ds.dataArray)-1), ds.dataArray[-self.xsize: -1])    
+            self.ax.set_xlim(len(ds.dataArray) - self.xsize, len(ds.dataArray))
+        else:
+            self.line.set_data(range(len(ds.dataArray)), ds.dataArray)
+            self.ax.set_xlim(0, len(ds.dataArray))
+            
+        self.ax.set_ylim(0, max(ds.dataArray) + 100)
 
-        self.ax.set_xlim(0, len(self.data))
-        self.ax.set_ylim(min(self.data) - 5, max(self.data) + 5)
-
-        #self.anomaly_points.set_data([i for i, x in enumerate(self.anomalies) if x], [self.data[i] for i in range(frame) if self.anomalies[i]])
+        #self.anomaly_points.set_data([i for i, x in enumerate(self.anomalies) if x], [ds.dataArray[i] for i in range(frame) if self.anomalies[i]])
         return self.line, self.anomaly_points
     
     def animate(self):
-        ani = FuncAnimation(self.fig, self.update, blit=True)
+        ani = FuncAnimation(self.fig, self.update, cache_frame_data=False)
         plt.show()
